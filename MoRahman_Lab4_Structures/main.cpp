c00a79b9abcf5d3b16ef9c63749bff04ee24d508
Mo Rahman <morahman7vnno2@gmail.com>
2016-11-27 04:03:01 -0800

    Overhauled entire application.
    The original program was fundamentally flawed; plain and simple:
    In an effort to avoid using the infamous "global variable", I made the task twice as hard as it needed to be. My first mistake was not initializing the testScore member of the StudentProfile structure as an array in the first place. This lead me to devise a crude solution; create a separate member variable and initialize that as an array to collect the scores...only that's not how structures quite work, I believe.
    
    Anyway, the resultant worked...but would cause memory leaks. Or something of that nature. Ether way, when the compiler has to break your code, that's usually a red flag that something is wrong.
    
    Global CONSTANT variables are the exception to the rule!
    
    Once I initialized the testScore member as an array, everything else seemed to effortlessly fall into place.
    
    Thank you Melvin, wherever the hell you are.

--- MoRahman_Lab4_Structures/main.cpp
+++ MoRahman_Lab4_Structures/main.cpp
@@ -1,64 +1,71 @@
-//------------------------------------------------------------------------
+//---------------------Mo Rahman 11/08/2016 11:35------------------------
 #include<iostream>
 #include<string>		//To use strings
 #include<iomanip>		//For set_precision<>()
 
+unsigned short int const NUM_TEST_SCORE = 4;		//The number of test scores per student
+
+//Global constant variables are the exception to the rule! ;)
+
 struct StudentProfile			//Structure tag: Profile of student
 {
-
-public:
-	unsigned short int testScore;					//Array of student's testscores
+	unsigned short int testScore[NUM_TEST_SCORE];	//Array of student's testscores
 	unsigned int studentID;							//To hold the student's ID number
 	std::string studentName;						//String to hold the student's name
-
 };
 
-//Function prototypes
+//FUNCTION PROTOTYPES
 
-//testScore, studentID, studentName; Pass by Value
-void getGrades(StudentProfile *, StudentProfile *, unsigned short int const*);
-//Pass by pointer, only the crunchiest
-float numCrunch(StudentProfile (&dummy) [4],unsigned short int const&);
+//testScore, studentID, studentName; Pass by Pointer
+void getGrades(StudentProfile *);
+//Pass by value, only the crunchiest
+float numCrunch(StudentProfile&);
+//Pass by value, pass the object and average
+void showResult (StudentProfile, float);
 
-//void showResult (StudentProfile, unsigned short int const);
+using namespace std;
 
 unsigned short int main()
 {
-	unsigned short int const NUM_TEST_SCORE = 4;	//Number of test scores per student
-	unsigned short int const *constPtr = &NUM_TEST_SCORE;
 	StudentProfile comSciStu;		//Computer Science Student
-	StudentProfile gradesFolder[NUM_TEST_SCORE];
-	//StudentProfile *gradesFolder = nullptr, *comSciStuPtr = nullptr;
+	char tryAgain;
+	float averageTop3 = 0.0f;		//For the numCrunch function
 
-	float averageTop3 = 0.0f;
+	cout << "This program generate a student profile. \n";
+	cout << "It will also generate the student's average test score; \n"
+		<< "based on their three highest scores." << endl;
+	do
+	{
+		//--------------------------------------------------------------------
 
-	std::cout << "This program generate a student profile. \n";
-	std::cout << "It will also generate the student's average test score." << std::endl;
+		getGrades(&comSciStu);
 
-	std::cout << "The size of gradesFolder before it goes into the getGrades function: ";
-	std::cout << sizeof(gradesFolder) << " bytes\n and the address is: " << &gradesFolder << std::endl;
+		//-------------------------------------------------------------------
 
-	std::cout << "However, the size of the testscore member variable is : ";
-	std::cout << sizeof(gradesFolder[0].testScore) << "bytes\n and the address is: " << &gradesFolder[0].testScore << std::endl;
+		averageTop3 = numCrunch(comSciStu);
 
-	std::cout << "By the way, the value of the testscore element is: " << gradesFolder[0].testScore << std::endl;
+		//-------------------------------------------------------------------
 
-	getGrades(&comSciStu, &gradesFolder[NUM_TEST_SCORE], &NUM_TEST_SCORE);
+		showResult(comSciStu, averageTop3);
 
-	std::cout << "Inside the main function. \n";
-	std::cout << gradesFolder[0].testScore << std::endl; std::cout << gradesFolder[1].testScore << std::endl;
-	std::cout << gradesFolder[2].testScore << std::endl; std::cout << gradesFolder[3].testScore << std::endl;
-	
-	averageTop3 = numCrunch(gradesFolder, NUM_TEST_SCORE);
+		//-------------------------------------------------------------------
+		//Ask the user if they want to try again, and
+		cout << "Would you like to try again? \n";
+		cout << "Please enter Y or N: \n";
+		cin >> tryAgain;
+		while (toupper(tryAgain) != 'Y' && toupper(tryAgain) != 'N')
+		{
+			cout << "Please enter Y or N: \n";
+			cin >> tryAgain;
+		}
+	} while (toupper(tryAgain) == 'Y');
 
-	system("pause");
+	//system("pause");
 	return 1;
 }
 
-//using namespace std;
-
 //Get the info for the student profile
-void getGrades(StudentProfile *comSciStu, StudentProfile *gradesFolder, unsigned short int const *NUM_TEST_SCORE)
+void getGrades(StudentProfile *comSciStu)
 {
 	
 	std::cout << "Hurry up and get the user data and do something with it! \n";
@@ -66,10 +73,18 @@ void getGrades(StudentProfile *comSciStu, StudentProfile *gradesFolder, unsigned
 	
 	getline(std::cin, (*comSciStu).studentName); std::cout << std::endl;
 
-	std::cout << "What is the student's ID number? :\n";
-	std::cin >> comSciStu -> studentID;
+	cout << "What is " << comSciStu->studentName << "'s ID number? :\n";
+	cin >> comSciStu -> studentID;
+
+	while (comSciStu->studentID >= 4294967295)
+	{
+		cout << "ID number too great. \n";
+		cout << "What is the grade of the test? : ";
+
+		cin >> comSciStu->studentID;
+	}
 	
-	for(unsigned short int counter = 0; counter != *NUM_TEST_SCORE; counter++)
+	for(unsigned short int counter = 0; counter != NUM_TEST_SCORE; counter++)
 	{
 		std::cout << "What is grade No."<< counter +1 << " of "
 				<< comSciStu ->studentName << "'s "<< counter +1;
@@ -82,33 +97,60 @@ void getGrades(StudentProfile *comSciStu, StudentProfile *gradesFolder, unsigned
 		else
 			std::cout << "th test ?\n";
 		
-		std::cin >> gradesFolder[counter].testScore;
+		std::cin >> (*comSciStu).testScore[counter];
 
-		while (gradesFolder[counter].testScore < 0 || gradesFolder[counter].testScore > 100)
+		while (comSciStu->testScore[counter] < 0 || comSciStu->testScore[counter] > 100)
 		{
-			std::cout << "Grades must range from 0 to 100.\n";
-			std::cout << "What is the grade of the test? : ";
-			std::cin >> gradesFolder[counter].testScore;
+			cout << "Grades must range from 0 to 100.\n";
+			cout << "What is the grade of the test? : ";
 
+			cin >> comSciStu->testScore[counter];
 		}
-
 	}
 	std::cout << "Inside the getGrades function. \n";
-	std::cout << gradesFolder[0].testScore << std::endl; std::cout << gradesFolder[1].testScore << std::endl;
-	std::cout << gradesFolder[2].testScore << std::endl; std::cout << gradesFolder[3].testScore << std::endl;
+	for (short counter = 0; counter < NUM_TEST_SCORE; counter++)
+	{
+		comSciStu->testScore[counter];
+	}
+
 }
 
-float numCrunch(StudentProfile  (&gradesFolder)[4], unsigned short int const &NUM_TEST_SCORE)
+//Calculate the average of the three highest scores
+float numCrunch(StudentProfile &comSciStu)
 {
-
-	std::cout << NUM_TEST_SCORE << std::endl;
-	std::cout << gradesFolder[0].testScore;
+	unsigned short int lowestScore, subTotal = 0;
 	
-	return 0.0f;
+	lowestScore = comSciStu.testScore[0];
+
+	for (short counter = 1; counter < NUM_TEST_SCORE; counter++)
+	{
+		if (comSciStu.testScore[counter] < comSciStu.testScore[counter + 2])
+		{
+			lowestScore = comSciStu.testScore[counter];
+		}
+	}
+
+	for (short counter = 0; counter < NUM_TEST_SCORE; counter++)
+	{
+		subTotal += comSciStu.testScore[counter];
+	}
+	
+	short total = (subTotal - lowestScore);
+
+	float average = static_cast<float>(total)/(NUM_TEST_SCORE - 1);
+
+	return average;
 }
-/*
-void showResult (StudentProfile &comSciStu, unsigned short int const &NUM_TEST_SCORE)
+
+//Display all of the data gathered
+void showResult (StudentProfile comSciStu, float avg)
 {
-	;
+	cout << "Name of student: \t \t \t " << comSciStu.studentName <<endl;
+	cout << "ID # of student: \t \t \t " << comSciStu.studentID << endl;
+	cout << "Test grades of student:\t\t\t" << comSciStu.testScore[0] << " "
+		<< comSciStu.testScore[1] << " " << comSciStu.testScore[2] << " "
+		<< comSciStu.testScore[3] << " " << endl;
+	cout << "The average of the three higest test scores: ";
+	cout << setprecision(2) << fixed << avg <<endl;
+	cout << "------------------------------------------------- \n";
 }
-*/
